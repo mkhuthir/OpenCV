831ed578c535a39b455cf6dc442ac70a55c244ff
mkhuthir <mkhuthir@MuthPC1>
2017-02-06 16:31:05 +0400

    added image

--- LinuxGCC_OpenCV_DisplayImage/src/DisplayImage.cpp
+++ LinuxGCC_OpenCV_DisplayImage/src/DisplayImage.cpp
@@ -13,19 +13,30 @@ using namespace cv;
 int main( int argc, char** argv )
 {
   Mat image;
+
+  if(argc<2)
+   {
+	  printf("\nUsage: ./DisplayImage [imagefile] !\n\nExiting..");
+	  return -1;
+   };
+
+  printf("Reading image file: %s\n",argv[1]);
   image = imread( argv[1], 1 );
 
-  if( argc != 2 || !image.data )
+  if( !image.data )
     {
-      printf( "No image data \n" );
+      printf( "No image data ! \n" );
       return -1;
-    }
+    };
 
   namedWindow( "Display Image", CV_WINDOW_AUTOSIZE );
   imshow( "Display Image", image );
 
   waitKey(0);
 
+  cvDestroyWindow("Display Image");
+
+  printf( "Exiting.. \n" );
   return 0;
 }
 
